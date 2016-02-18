73dc85d103a205dc0b1be4bd9496c1c5957e8c19
Brett Vickers <brett@beevik.com>
2016-02-18 14:39:17 -0800

    Begin work on logging module.

---
+++ include/kernel/debug/log.h
@@ -0,0 +1,60 @@
+//============================================================================
+/// @file       log.c
+/// @brief      Kernel logging module.
+//
+//  Copyright 2016 Brett Vickers.
+//  Use of this source code is governed by a BSD-style license
+//  that can be found in the MonkOS LICENSE file.
+//============================================================================
+
+#pragma once
+
+#include <core.h>
+#include <stdarg.h>
+
+//----------------------------------------------------------------------------
+//  @enum       loglevel_t
+/// @brief      A log level indicates the importance of a logged message.
+//----------------------------------------------------------------------------
+typedef enum loglevel
+{
+    LOG_CRIT,       ///< Critical error, occurs just prior to crashing
+    LOG_ERR,        ///< Serious error in software or hardware
+    LOG_WARNING,    ///< Warning about a significant issue
+    LOG_INFO,       ///< Informational message
+    LOG_DEBUG,      ///< Used for kernel debugging
+    LOG_DEFAULT,    ///< Default kernel logging level
+} loglevel_t;
+
+//----------------------------------------------------------------------------
+//  @function   log
+/// @brief      Log a message to the kernel log buffer.
+/// @param[in]  level   The importance level of the logged message.
+/// @param[in]  str     The null-terminated string to be printed.
+//----------------------------------------------------------------------------
+void
+log(loglevel_t level, const char *str);
+
+//----------------------------------------------------------------------------
+//  @function   logf
+/// @brief      Log a printf-formatted message to the kernel log buffer.
+/// @param[in]  level   The importance level of the logged message.
+/// @param[in]  format  The null-terminated format string used to format the
+///                     text to be printed.
+/// @param[in]  ...     Variable arguments list to be initialized with
+///                     va_start.
+//----------------------------------------------------------------------------
+void
+logf(loglevel_t level, const char *format, ...);
+
+//----------------------------------------------------------------------------
+//  @function   logvf
+/// @brief      Log a printf-formatted string using a variable argument list.
+/// @param[in]  level   The importance level of the logged message.
+/// @param[in]  format  The null-terminated format string used to format the
+///                     text to be printed.
+/// @param[in]  arg     Variable arguments list to be initialized with
+///                     va_start.
+//----------------------------------------------------------------------------
+void
+logvf(loglevel_t level, const char *format, va_list args);
---
+++ kernel/debug/log.c
@@ -0,0 +1,164 @@
+//============================================================================
+/// @file       log.c
+/// @brief      Kernel logging module.
+//
+//  Copyright 2016 Brett Vickers.
+//  Use of this source code is governed by a BSD-style license
+//  that can be found in the MonkOS LICENSE file.
+//============================================================================
+
+#include <libc/stdio.h>
+#include <libc/string.h>
+#include <kernel/debug/log.h>
+
+// Record buffer constants
+#define RBUFSHIFT  10
+#define RBUFSIZE   (1 << RBUFSHIFT)     // 1024
+#define RBUFMASK   (RBUFSIZE - 1)
+
+// Message buffer constants
+#define MBUFSHIFT  7
+#define MBUFSIZE   (1 << MBUFSHIFT)     // 64KiB
+#define MBUFMASK   (MBUFSIZE - 1)
+
+/// A log record represents a single logged event.
+typedef struct record
+{
+    loglevel_t level;
+    int        moffset;      ///< Offset of message in context::mbuf
+} record_t;
+
+/// The log context describes the state of the log buffers. There are two
+/// buffers: the record buffer and the message buffer. The record buffer is a
+/// circular queue that contains records describing the most recent 1024 log
+/// messages. The message buffer contains the text messages associated with
+/// each of the records in the log buffer.
+struct context
+{
+    record_t rbuf[RBUFSIZE]; ///< Circular record buffer
+    int      rhead;          ///< index of oldest record in rbuf
+    int      rtail;          ///< rbuf index where next record will be written
+    int      rbufsz;         ///< number of records in rbuf
+
+    char     mbuf[MBUFSIZE]; ///< Circular message text buffer
+    int      mhead;          ///< index of oldest char in mbuf
+    int      mtail;          ///< mbuf index where next char will be written
+    int      mbufsz;         ///< the number of characters in mbuf
+};
+
+static struct context lc;
+
+static void
+evict_record()
+{
+    lc.rhead = (lc.rhead + 1) & RBUFMASK;
+    lc.rbufsz--;
+}
+
+static void
+evict_msg(int chars)
+{
+    // Evict the requested number of characters from the mbuf. Evict
+    // associated records when the end of a message is reached.
+    for (int i = 0; i < chars && lc.mbufsz > 0; i++) {
+        if (lc.mbuf[lc.mhead] == 0)
+            evict_record();
+        lc.mhead = (lc.mhead + 1) & MBUFMASK;
+        lc.mbufsz--;
+    }
+
+    // Keep evicting characters from the message buffer until a null
+    // terminator is reached.
+    while (lc.mbufsz > 0) {
+        char ch = lc.mbuf[lc.mhead];
+        lc.mhead = (lc.mhead + 1) & MBUFMASK;
+        lc.mbufsz--;
+        if (ch == 0) {
+            evict_record();
+            break;
+        }
+    }
+}
+
+static void
+evict_oldestmsg()
+{
+    while (lc.mbufsz > 0) {
+        char ch = lc.mbuf[lc.mhead];
+        lc.mhead = (lc.mhead + 1) & MBUFMASK;
+        lc.mbufsz--;
+        if (ch == 0)
+            break;
+    }
+}
+
+static int
+add_string(const char *str)
+{
+    int offset = lc.mtail;
+
+    // Evict one or more log records if we're going to exceed the buffer size.
+    int schars = strlen(str) + 1;
+    if (lc.mbufsz + schars > MBUFSIZE)
+        evict_msg(lc.mbufsz + schars - MBUFSIZE);
+
+    // Copy the string into the buffer, handling wrap-around.
+    if (lc.mtail + schars > MBUFSIZE) {
+        int split = MBUFSIZE - lc.mtail;
+        memcpy(lc.mbuf + lc.mtail, str, split);
+        memcpy(lc.mbuf, str + split, schars - split);
+        lc.mtail = schars - split;
+    }
+    else {
+        memcpy(lc.mbuf + lc.mtail, str, schars);
+        lc.mtail += schars;
+    }
+
+    lc.mbufsz += schars;
+
+    return offset;
+}
+
+static void
+add_record(int offset, loglevel_t level)
+{
+    // If the record buffer is full, evict the oldest record and its
+    // associated message text.
+    if (lc.rbufsz == RBUFSIZE) {
+        evict_record();
+        evict_oldestmsg();
+    }
+
+    // Add a new record on the tail of the circular record queue.
+    record_t *r = lc.rbuf + lc.rtail;
+    r->moffset = offset;
+    r->level   = level;
+
+    lc.rtail = (lc.rtail + 1) & RBUFMASK;
+    lc.rbufsz++;
+}
+
+void
+log(loglevel_t level, const char *str)
+{
+    int offset = add_string(str);
+    add_record(offset, level);
+}
+
+void
+logf(loglevel_t level, const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    logvf(level, format, args);
+    va_end(args);
+}
+
+void
+logvf(loglevel_t level, const char *format, va_list args)
+{
+    char str[1024];
+    vsnprintf(str, arrsize(str), format, args);
+    int offset = add_string(str);
+    add_record(offset, level);
+}
